9b0fb53f1d9371ce0c9ef18f303331cdd5a9c9d7
Kushagra Gangwar <kgang6434@gmail.com>
2024-07-29 16:16:47 +0530

    ui updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("\u00D7 ");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
@@ -46,6 +46,7 @@ function newElement() {
     alert("You must write something!");
   } else {
     li.appendChild(icon);
+    li.append(" ");
     li.appendChild(t);
     document.getElementById("myUL").appendChild(li);
   }
